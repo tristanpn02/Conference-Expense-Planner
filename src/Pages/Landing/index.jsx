e1e27487ac771c8442a77ddcc1864ef8eaf7eaa6
Tristan Andersen <tpan02@pm.me>
2024-10-14 23:39:23 +0000

    Create a simple landing page

---
+++ src/Pages/Landing/index.jsx
@@ -0,0 +1,11 @@
+const LandingPage = () => {
+
+    return (
+        <div className="landing-page">
+            <h1>Conference Expense Planner</h1>
+            <p>Welcome to Spilum.Net's Conference Expense Planner. With this tool you can do business things =)</p>
+        </div>
+    )
+}
+
+export default LandingPage;
