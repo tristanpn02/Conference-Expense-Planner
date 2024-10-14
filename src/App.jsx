92b7a4bd365d474db8b37082cbe0535491e293f7
Tristan Andersen <tpan02@pm.me>
2024-10-14 23:53:54 +0000

    Clean-up css and add background for landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import LandingPage from './Pages/Landing'
 
@@ -9,7 +7,7 @@ function App() {
 
   return (
     <>
-      <div className='app'>
+      <div className='Main'>
         <LandingPage />
       </div>
     </>
--- src/Pages/Landing/index.jsx
+++ src/Pages/Landing/index.jsx
@@ -1,7 +1,9 @@
+import background from './tired-asian-business-woman-stress.webp';
+
 const LandingPage = () => {
 
     return (
-        <div className="landing-page">
+        <div className="landing-page" style={{ backgroundImage: `url(${background})` }}>
             <h1>Conference Expense Planner</h1>
             <p>Welcome to Spilum.Net's Conference Expense Planner. With this tool you can do business things =)</p>
         </div>
