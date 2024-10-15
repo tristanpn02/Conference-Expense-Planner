9745e97d77a33f7ca91c05e1f8b1dd887b1d04b0
Tristan Andersen <tpan02@pm.me>
2024-10-15 00:12:15 +0000

    Change layout and alignment of landing page

--- src/Pages/Landing/index.jsx
+++ src/Pages/Landing/index.jsx
@@ -4,8 +4,15 @@ const LandingPage = () => {
 
     return (
         <div className="landing-page">
-            <h1>Conference Expense Planner</h1>
-            <p>Welcome to Spilum.Net's Conference Expense Planner. With this tool you can do business things =)</p>
+            <div className="details">
+                <div className="title">
+                    <h1>Conference Expense Planner</h1><br />
+                    <button>Get Started</button>
+                </div>
+                <div className="info">
+                    <p>Welcome to Spilum.Net's Conference Expense Planner. With this tool you can do business things =)</p>
+                </div>
+            </div>
         </div>
     )
 }
