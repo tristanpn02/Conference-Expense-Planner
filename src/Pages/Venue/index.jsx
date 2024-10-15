f769a29b681b4f6b51fda72ab26e9218b3e38091
Tristan Andersen <tpan02@pm.me>
2024-10-15 01:44:16 +0000

    Create skeleton of VenuePage

---
+++ src/Pages/Venue/index.jsx
@@ -0,0 +1,7 @@
+const VenuePage = () => {
+    return (
+        <h1>hi =3</h1>
+    )
+}
+
+export default VenuePage;
--- src/routesConfig.tsx
+++ src/routesConfig.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import LandingPage from "./Pages/Landing";
+import VenuePage from "./Pages/Venue";
 
 const routesConfig = [
     { path: '/home', title: 'Home', component: <LandingPage />, navbar: true },
+    { path: '/venue', title: 'Venue', component: <VenuePage />, navbar: true},
 ];
 
 export default routesConfig;
