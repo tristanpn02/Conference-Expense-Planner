f3e2ffece9c06443fa8649fd2f528a73f53a857c
Tristan Andersen <tpan02@pm.me>
2024-10-15 21:40:18 +0000

    Make VenueBlock into a button

--- src/Components/VenueBlock/index.jsx
+++ src/Components/VenueBlock/index.jsx
@@ -3,13 +3,13 @@ import './styles.css'
 const VenueBlock = ({venue}) => {
 
     return (
-        <div className="venue-block" style={{backgroundImage: `url(${venue.img})`}}>
+        <button className="venue-block" style={{backgroundImage: `url(${venue.img})`}}>
             <div className="details">
                 <h2>{venue.name}</h2>
                 <p>Capacity: {venue.capacity}</p>
                 <p>Price: ${venue.price}</p>
             </div>
-        </div>
+        </button>
     )
 }
 
