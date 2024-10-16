4830b57620cb1435ca5c94d81af127cee065ca15
Tristan Andersen <tpan02@pm.me>
2024-10-16 02:01:01 +0000

    Background coloring

--- src/Pages/Landing/index.jsx
+++ src/Pages/Landing/index.jsx
@@ -11,8 +11,8 @@ const LandingPage = () => {
                     <h1>Conference Expense Planner</h1>
                     <p>Welcome to Spilum.Net's Conference Expense Planner. With this tool you can do business things =)</p>
                     <br />
-                    <Link to="/venue">
-                     <button>Get Started</button>
+                    <Link to="/planner">
+                     <button className='get-started-btn'>Get Started</button>
                     </Link>
                 </div>
             </div>
